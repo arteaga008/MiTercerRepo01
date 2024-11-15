db34d64f12f1da4f5b5c40e7c287fd6e8af94dda
Amin Arteaga <arteaga_manuel08@hotmail.com>
2024-11-15 12:50:18 -0600

    Se creo el archivo py. Y su contenido

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo01",                              # Nombre del paquete
+    version="0.1.0",                                  # Versión inicial
+    packages=find_packages(),                         # Paquetes a incluir
+    description="Un paquete pip simple de saludo",    # Breve descripción
+    author="Manuel Artega",                           # Tu nombre
+    author_email="arteaga_manuel08@hotmail.com",      # Tu correo electrónico
+    url="https://github.com/arteaga008/tercerRepo01", # URL del proyecto
+)
